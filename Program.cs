e10e5d23eadb25d18a9284d3c446e7610c109663
German Vassiljev <93324304+GermanVassiljev@users.noreply.github.com>
2022-03-09 15:37:02 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,28 +10,43 @@ namespace Klasside_loomine
     {
         static void Main(string[] args)
         {
-            Inimene naine = new Inimene("Kask");
-            naine.Vanus = int.Parse(Console.ReadLine());
-            naine.Tervitamine();
+            Inimene i = new Opilane("Anna", "Tamm", "TARpe21");
+            i.Kes_ma_olen();
+            i.Tervitamine();
 
-            Inimene mees = new Inimene();
-            mees.Perenimi = "Tamm";
-            mees.Vanus = 45;
-            Console.WriteLine(mees.Perenimi, " on ", mees.Vanus, " aastane mees " );
-            mees.Tervitamine();
-            Inimene[] inimesed = new Inimene[5];
-            for (int i = 0; i < 5; i++)
-            {
-                inimesed[i] = new Inimene();
-                Console.WriteLine("Mis on sinu nimi?");
-                inimesed[i].Perenimi = Console.ReadLine();
-                Console.WriteLine("Kui vana sa oled?");
-                inimesed[i].Vanus = int.Parse(Console.ReadLine());
-            }
-            foreach (Inimene inimene in inimesed)
-            {
-                inimene.Tervitamine();
-            }
+            Inimene[] inim = new Inimene[4];
+            inim[0] = new Opetaja();
+            inim[0].Kes_ma_olen();
+            inim[1] = new Opilane();
+            inim[1].Kes_ma_olen();
+            inim[2] = new Opetaja();
+            inim[2].Kes_ma_olen();
+            inim[3] = new Opilane();
+            inim[3].Kes_ma_olen();
+            //Inimene naine = new Inimene("Kask"," ");
+            //naine.Vanus = int.Parse(Console.ReadLine());
+            //naine.Tervitamine();
+
+            //Inimene mees = new Inimene();
+            //mees.Perenimi = "Tamm";
+            //mees.Vanus = 45;
+            //Console.WriteLine(mees.Perenimi, " on ", mees.Vanus, " aastane mees " );
+            //mees.Tervitamine();
+            //Inimene[] inimesed = new Inimene[5];
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    inimesed[i] = new Inimene();
+            //    Console.WriteLine("Mis on sinu nimi?");
+            //    inimesed[i].Perenimi = Console.ReadLine();
+            //    Console.WriteLine("Kui vana sa oled?");
+            //    inimesed[i].Vanus = int.Parse(Console.ReadLine());
+            //}
+            //foreach (Inimene inimene in inimesed)
+            //{
+            //    inimene.Tervitamine();
+            //}
+            //Opilane Jaan = new Opilane("Jaan","Puu","LOGpe55");
+
             Console.ReadKey();
         }
     }
