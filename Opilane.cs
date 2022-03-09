3ec25fba3a0696a1e895f3f8166a0d5cf66ee16b
German Vassiljev <93324304+GermanVassiljev@users.noreply.github.com>
2022-03-09 15:37:54 +0200

    Create Opilane.cs

---
+++ Opilane.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasside_loomine
+{
+    public class Opilane : Inimene
+    {
+        string ruhm;
+        double hinne;
+        bool toetus;
+        public Opilane() { }
+        public Opilane(string Eesnimi, string Perenimi, string Ruhm):base(Eesnimi,Perenimi)
+        {
+            ruhm = Ruhm;
+        }
+        public string Ruhm
+        {
+            set { ruhm = value; }
+            get { return ruhm; }
+        }
+        public double Hinne
+        {
+            set { if (value > 0 && value <= 5) hinne = value; }
+            get { return hinne; }
+        }
+        public bool Toetus
+        {
+            get
+            {
+                if (hinne > 3)
+                {
+                    toetus = true;
+                }
+                else
+                {
+                    toetus = false;
+                }
+                return toetus;
+            }
+        }
+        public void Informatsioon()
+        {
+            string t = "";
+            Console.WriteLine("Tere! Minu nimi on {0} {1}",Eesnimi, Perenimi);
+            if (Toetus==true)
+            {
+                t = "Saan toetust kätte €_€";
+            }
+            else if (Toetus==false && hinne>0)
+            {
+                t = "Ei saa toetus ;_;";
+            }
+            else
+            {
+                t = "Andmed hinnetest puuduvad 0_0";
+            }
+            Console.WriteLine("Minu keskmine hinne on {0}. {1}",hinne, t);
+        }
+        public override void Kes_ma_olen()
+        {
+            Console.WriteLine("Ma olen opilane!");
+        }
+    }
+}
